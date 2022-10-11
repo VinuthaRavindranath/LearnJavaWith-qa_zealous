e0547c819b934a1fb5c3525159865405089aa032
Vinutha R <vinuthar@Vinuthas-MacBook-Pro.local>
2022-10-11 17:27:58 +0530

    WhileLoopPrograms

---
+++ DoWhileLoopExamples/Lesson1.java
@@ -0,0 +1,10 @@
+package DoWhileLoopExamples;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        int i =20;
+        do {
+            System.out.println(i++);
+        } while (i<=10);
+    }
+}
---
+++ WhileLoopExamples/Lesson2.java
@@ -0,0 +1,10 @@
+package WhileLoopExamples;
+//Display Numbers from 1 to 5
+public class Lesson2 {
+    public static void main(String[] args) {
+        int n =1;
+        while (n<=5) {
+            System.out.println(n++);
+        }
+    }
+}
